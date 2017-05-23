ad274b09f4da2dfa541400a65fce4ee7a31d9ec1
ttreat1 <ttreat@paloaltonetworks.com>
2017-05-22 21:39:39 -0700

    Commit5

--- source/conf.py
+++ source/conf.py
@@ -91,11 +91,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
